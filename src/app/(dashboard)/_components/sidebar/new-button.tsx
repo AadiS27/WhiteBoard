09b5b5919db8a0a40701f300d39a6c7b68f305c8
AadiS27 <Aadisinghal0591@gmail.com>
2025-05-07 19:06:27 +0530

    Kuch toh kiya shyd added sidebar

--- src/app/(dashboard)/_components/sidebar/new-button.tsx
+++ src/app/(dashboard)/_components/sidebar/new-button.tsx
@@ -1,8 +1,36 @@
+import { Plus } from "lucide-react"
+import { CreateOrganization } from "@clerk/nextjs"
+
+import {Dialog,
+    DialogTrigger,
+    DialogContent,
+    DialogTitle
+}from "@/components/ui/dialog";
+import { Hint } from "@/components/hint";
+
 export const NewButton = () => {
     return(
-        <div className=" text-white font-bold py-2 px-4 rounded">
-            New
+        <Dialog>
+            <DialogTrigger asChild>
+                <div className="aspect-square">
+                    <Hint label="Create Organization" side="right" align="start" sideOffset={10} alignOffset={-10}>
+                    <button className="bg-white/25 h-full w-full rounded-md items-center justify-center opacity-80 hover:opacity-100 flex transition-all duration-200 ease-in-out">
+                        <Plus className="text-white" size={24}/>
+                    </button>
+                    </Hint>
+                </div>
+            </DialogTrigger>
+            <DialogContent className="p-0 bg-white border-none overflow-hidden">
+        <div className="rounded-md shadow-lg">
+          <DialogTitle className="text-black p-4">
+            Create Organization
+          </DialogTitle>
+          <div className=" pl-9 pt-7 pb-7">
+            <CreateOrganization />
+          </div>
         </div>
+      </DialogContent>
+        </Dialog>
 
     )
 }
